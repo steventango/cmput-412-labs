c11294604c4ee518e1dff532071b536eff7942ae
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-01-22 15:15:39 -0700

    B-6

---
+++ exercise-1/pondcleaner-library/src/duckietown_pondcleaner/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "1.0.5"
+
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+from .pondcleaner import *
+from .demo import *
