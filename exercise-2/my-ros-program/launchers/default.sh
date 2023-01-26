2d4d4cf6d477a4263fc541a0cb00a4c6543d02ce
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-01-25 23:53:00 -0700

    C-2.4

--- exercise-2/my-ros-program/launchers/default.sh
+++ exercise-2/my-ros-program/launchers/default.sh
@@ -14,6 +14,7 @@ dt-launchfile-init
 
 # launching app
 dt-exec rosrun my_package my_publisher_node.py
+dt-exec rosrun my_package my_subscriber_node.py
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
