28bd05bacd9586254fe3a110619a89f1e0af30f2
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-01-26 17:47:44 -0700

    Q2

---
+++ exercise-2/my-ros-program/packages/my_package/src/my_image_node.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+
+import cv2
+import numpy as np
+import rospy
+from duckietown.dtros import DTROS, NodeType
+from sensor_msgs.msg import CompressedImage
+
+
+class MyImageNode(DTROS):
+
+    def __init__(self, node_name):
+        # initialize the DTROS parent class
+        super(MyImageNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
+        self.hostname = os.environ.get('VEHICLE_NAME')
+        self.pub = rospy.Publisher(f"~image/compressed", CompressedImage, queue_size=1)
+        self.sub = rospy.Subscriber(f'camera_node/image/compressed', CompressedImage, self.callback)
+
+    def callback(self, data):
+        array = np.frombuffer(data.data, dtype=np.uint8)
+        image = cv2.imdecode(array, cv2.IMREAD_COLOR)
+        rospy.loginfo(f"Size of the image {image.shape}")
+        self.pub.publish(data)
+
+if __name__ == '__main__':
+    # create the node
+    node = MyImageNode(node_name='my_subscriber_node')
+    # keep spinning
+    rospy.spin()
